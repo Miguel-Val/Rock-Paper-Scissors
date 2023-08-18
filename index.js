6be55ddacd765d0e3d980e96d6a5bf305a1d9ac7
Miguel-Val <chahpo@gmail.com>
2023-08-18 16:24:58 -0400

    Change random choice logic, more cleaner

--- index.js
+++ index.js
@@ -1,63 +1,94 @@
 
+let choice = ['Rock', 'Paper', 'Scissors']
+
+let computerChoice = Math.floor(Math.random() * choice.length);
+
+console.log(choice[computerChoice]);
+
+// function getComputerChoice (rock,paper,scissors) {
+
+//     let rdm = Math.floor(Math.random() * 3) + 1;
+
+//     if (rdm === 1) {
+//         return(rock);
+//     }
+//     else if (rdm === 2) {
+//         return(paper);
+//     }
+//     else if (rdm === 3) {
+//         return(scissors);
+//     }
+// }
+
+let pRock;
+let pScissors;
+let pPaper;
 
 
-function getComputerChoice (rock,paper,scissors) {
+// const rock = document.querySelector('.rock');
 
-    let rdm = Math.floor(Math.random() * 3) + 1;
+// rock.addEventListener('click', function funcRock (){
+//     return 'Rock';
+// });
 
-    if (rdm === 1) {
-        return(rock);
-    }
-    else if (rdm === 2) {
-        return(paper);
-    }
-    else if (rdm === 3) {
-        return(scissors);
-    }
+
+// const paper = document.querySelector('.paper');
+
+// paper.addEventListener('click', function funcPaper (){
+//     return 'Paper';
+// });
+
+
+
+function cos () {
+    let pScissors = 'Scissors';
+    const scissors = document.querySelector('.scissors');
+
+    scissors.addEventListener('click', function (pScissors) {
+        pScissors = "Scissors";
+        
+
+        
+    });
 }
 
 
 
 
 
-function playerSelection (getComputerChoice) {
-    let plyr = prompt("Please type in your selection, Rock, Paper, Scissors. ");
-    
 
-    if (plyr.toLowerCase() == getComputerChoice.toLowerCase()) {
+
+
+function playRound(funcPaper, getComputerChoice){
+    
+    if (funcPaper == getComputerChoice.toLowerCase()) {
         console.log("Tie!");
         
     }
-    if (plyr.toLowerCase() == 'rock' && getComputerChoice.toLowerCase() == 'scissors'){
+    if (playerSelection == 'rock' && getComputerChoice.toLowerCase() == 'scissors'){
         console.log(`You Win! ${plyr} beats ${getComputerChoice}`)
         
     }
-    else if (plyr.toLowerCase() == 'scissors' && getComputerChoice.toLowerCase() == 'rock') {
+    else if (playerSelection == 'scissors' && getComputerChoice.toLowerCase() == 'rock') {
         console.log(`You Lose! ${getComputerChoice} beats ${plyr}`)
 
     }
-    else if (plyr.toLowerCase() == 'scissors' && getComputerChoice.toLowerCase() == 'paper') {
+    else if (playerSelection == 'scissors' && getComputerChoice.toLowerCase() == 'paper') {
         console.log(`You Win! ${plyr} beats ${getComputerChoice}`)
     }
-    else if (plyr.toLowerCase() == 'paper' && getComputerChoice.toLowerCase() == 'scissors') {
+    else if (playerSelection == 'paper' && getComputerChoice.toLowerCase() == 'scissors') {
         console.log(`You Lose! ${getComputerChoice} beats ${plyr}`)
     }
-    if (plyr.toLowerCase() == 'paper' && getComputerChoice.toLowerCase() == 'rock'){
+    if (playerSelection == 'paper' && getComputerChoice.toLowerCase() == 'rock'){
         console.log(`You Win! ${plyr} beats ${getComputerChoice}`)
         
     }
-    else if (plyr.toLowerCase() == 'rock' && getComputerChoice.toLowerCase() == 'paper') {
+    else if (playerSelection == 'rock' && getComputerChoice.toLowerCase() == 'paper') {
         console.log(`You Lose! ${getComputerChoice} beats ${plyr}`)
     
-}
+    }
 
 }
 
 
 
-
-playerSelection(getComputerChoice("Rock", "Paper", "Scissors"));
-playerSelection(getComputerChoice("Rock", "Paper", "Scissors"));
-playerSelection(getComputerChoice("Rock", "Paper", "Scissors"));
-playerSelection(getComputerChoice("Rock", "Paper", "Scissors"));
-playerSelection(getComputerChoice("Rock", "Paper", "Scissors"));
